e831e1035f5aef71bfdfe1b33f82bef630b35bfd
jaimeaq <123690985+jaimeaq@users.noreply.github.com>
2024-05-29 23:05:14 -0400

    Initial commit. Before Displaying product details.

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  template: "<store></store>",
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'SportsStore';
+}
