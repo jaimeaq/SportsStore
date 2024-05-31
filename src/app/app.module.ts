3ff8b5804fab54998dbdeeada924eb99ed1d6ee6
jaimeaq <123690985+jaimeaq@users.noreply.github.com>
2024-05-31 18:02:21 -0400

    URL routing added. NEXT: Complete cart detail feature.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import {StoreModule} from "./store/store.module";
+import {RouterModule} from "@angular/router";
+import {StoreComponent} from "./store/store.component";
+import {CartDetailComponent} from "./store/cartDetail.component";
+import {CheckoutComponent} from "./store/checkout.component";
+import {StoreFirstGuard} from "./storeFirst.guard";
 
 @NgModule({
   declarations: [
@@ -10,9 +15,24 @@ import {StoreModule} from "./store/store.module";
   ],
   imports: [
     BrowserModule,
-    StoreModule
+    StoreModule,
+    RouterModule.forRoot([
+      {
+        path: "store", component: StoreComponent,
+        canActivate: [StoreFirstGuard]
+      },
+      {
+        path: "cart", component: CartDetailComponent,
+        canActivate: [StoreFirstGuard]
+      },
+      {
+        path: "checkout", component: CheckoutComponent,
+        canActivate: [StoreFirstGuard]
+      },
+      { path: "**", redirectTo: "/store" }
+    ])
   ],
-  providers: [],
+  providers: [StoreFirstGuard],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
