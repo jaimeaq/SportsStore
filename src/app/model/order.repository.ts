90cdba2e2a4f3e25a0a47042fff28640662e80ff
jaimeaq <123690985+jaimeaq@users.noreply.github.com>
2024-06-12 17:12:52 -0400

    Data source and repositories extended to perform authentication before sending requests to the RESTful web service. NEXT: Install component library (Angular Material 16.0.0)

--- src/app/model/order.repository.ts
+++ src/app/model/order.repository.ts
@@ -1,15 +1,55 @@
-import {Injectable} from "@angular/core";
+import {Injectable, signal} from "@angular/core";
 import {Observable} from "rxjs";
 import {Order} from "./order.model";
 import {RestDataSource} from "./rest.datasource";
 
 @Injectable()
 export class OrderRepository {
+  private ordersSignal = signal<Order[]>([]);
+  private loaded: boolean = false;
 
   constructor(private dataSource: RestDataSource) {
   }
 
+  loadOrders() {
+    this.loaded = true;
+    this.dataSource.getOrders().subscribe(data => {
+      this.ordersSignal.set(data);
+    })
+  }
+
+  get orders() {
+    if (!this.loaded) {
+      this.loadOrders();
+    }
+    return this.ordersSignal.asReadonly();
+  }
+
   saveOrder(order: Order): Observable<Order> {
+    this.loaded = false;
     return this.dataSource.saveOrder(order);
+  }
+
+  updateOrder(order: Order) {
+    this.dataSource.updateOrder(order).subscribe(updatedOrder => {
+      this.ordersSignal.update(orders => {
+        const index = orders.findIndex(o => o.id === updatedOrder.id);
+        if (index !== -1) {
+          const updatedOrders = [...orders]; // Clone the array
+          updatedOrders.splice(index, 1, updatedOrder); // Replace the order at the found index
+          return updatedOrders;
+        }
+        return orders; // Return the original array if the order is not found
+      });
+    });
+  }
+
+  deleteOrder(id: number) {
+    this.dataSource.deleteOrder(id).subscribe(() => {
+      this.ordersSignal.update(orders => {
+        const updatedOrders = orders.filter(o => o.id !== id); // Create a new array excluding the order with the given id
+        return updatedOrders;
+      });
+    });
   }
 }
--- src/app/model/product.repository.ts
+++ src/app/model/product.repository.ts
@@ -1,17 +1,16 @@
-import {computed, Injectable, Signal} from "@angular/core";
+import {computed, Injectable, signal, Signal} from "@angular/core";
 import {Product} from "./product.model";
 import {RestDataSource} from "./rest.datasource";
-import {toSignal} from "@angular/core/rxjs-interop";
 
 @Injectable()
 export class ProductRepository {
-  products: Signal<Product[]>
+  products = signal<Product[]>([]);
   categories: Signal<string[]>;
 
   constructor(private dataSource: RestDataSource) {
-    this.products = toSignal(dataSource.products, {
-      initialValue: []
-    });
+    dataSource.products.subscribe(data => {
+      this.products.set(data);
+    })
     this.categories = computed(() => {
       return this.products()
         .map(p => p.category ?? "(None)")
@@ -19,7 +18,38 @@ export class ProductRepository {
     })
   }
 
-  getProducts(id: number): Product | undefined {
+  getProduct(id: number): Product | undefined {
     return this.products().find(p => p.id == id);
+  }
+
+  saveProduct(product: Product) {
+    if (product.id == null || product.id == 0) {
+      this.dataSource.saveProduct(product)
+        .subscribe(p => {
+          this.products.update(pdata => [...pdata, p]); // Append the new product
+        });
+    } else {
+      this.dataSource.updateProduct(product)
+        .subscribe(p => {
+          this.products.update(pdata => {
+            const index = pdata.findIndex(p => p.id == product.id);
+            if (index !== -1) {
+              const updatedProducts = [...pdata]; // Clone the array
+              updatedProducts.splice(index, 1, product); // Replace the product at the found index
+              return updatedProducts;
+            }
+            return pdata; // Return the original array if the product is not found
+          });
+        });
+    }
+  }
+
+  deleteProduct(id: number) {
+    this.dataSource.deleteProduct(id).subscribe(() => {
+      this.products.update(pdata => {
+        const updatedProducts = pdata.filter(p => p.id !== id); // Create a new array excluding the product with the given id
+        return updatedProducts;
+      });
+    });
   }
 }
